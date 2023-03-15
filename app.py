f041e2fdcc8aa4c9b0eb472a2b9b6db802f183f2
louis N <louis_n@naver.com>
2023-03-15 14:40:10 +0900

    [추가,변경] temp.html 변경, member_detail4 추가, app.py 변경

--- app.py
+++ app.py
@@ -59,8 +59,9 @@ def get_guest_book():
     return jsonify({'guest_book_list': guest_book_list})
 
 
-@app.route("/card/<int:member_num>", methods=["POST"])
-def save_card(member_num):
+@app.route("/card", methods=["POST"])
+def save_card():
+    member_num = request.form['member_num_give']
     card_title_receive = request.form['card_title_give']
     card_text_receive = request.form['card_text_give']
     card_img_receive = request.form['card_img_give']
@@ -73,16 +74,44 @@ def save_card(member_num):
             'card_img': card_img_receive,
             'member_num': member_num
         }
-        db.member+member_num.insert_one(doc)
-        return jsonify({'msg': '등록 완료'})
+
+        if member_num == 1:
+            db.member_1.insert_one(doc)
+        elif member_num == 2:
+            db.member_2.insert_one(doc)
+        elif member_num == 3:
+            db.member_3.insert_one(doc)
+        elif member_num == 4:
+            db.member_4.insert_one(doc)
+        elif member_num == 5:
+            db.member_5.insert_one(doc)
+        else:
+            db.member_6.insert_one(doc)
+
+        return jsonify({'msg': '등록 완료', 'reload': '1'})
     else:
-        return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.', 'reload': '0'})
 
 
-@app.route("/card/<int:member_num>", methods=["GET"])
-def get_card(member_num):
+@app.route("/card", methods=["GET"])
+def get_card():
+    member_num = request.args.get('num')
+
+    if member_num == 1:
+        target_db = db.member_1.find()
+    elif member_num == 2:
+        target_db = db.member_2.find()
+    elif member_num == 3:
+        target_db = db.member_3.find()
+    elif member_num == 4:
+        target_db = db.member_4.find()
+    elif member_num == 5:
+        target_db = db.member_5.find()
+    else:
+        target_db = db.member_6.find()
+
     card_list = []
-    for doc in db.member+member_num.find():
+    for doc in target_db:
         doc['_id'] = str(doc['_id'])
         card_list.append(doc)
 
@@ -91,9 +120,49 @@ def get_card(member_num):
 
 @app.route("/detail/<int:member_num>/<string:object_id>", methods=["GET"])
 def get_card_detail(member_num, object_id):
-    card = db.member+member_num.find_one({'_id': ObjectId(object_id)})
+    if member_num == 1:
+        temp_card = db.member_1.find_one({'_id': ObjectId(object_id)})
+    elif member_num == 2:
+        temp_card = db.member_2.find_one({'_id': ObjectId(object_id)})
+    elif member_num == 3:
+        temp_card = db.member_3.find_one({'_id': ObjectId(object_id)})
+    elif member_num == 4:
+        temp_card = db.member_4.find_one({'_id': ObjectId(object_id)})
+    elif member_num == 5:
+        temp_card = db.member_5.find_one({'_id': ObjectId(object_id)})
+    else:
+        temp_card = db.member_6.find_one({'_id': ObjectId(object_id)})
+
+    temp_card['_id'] = str(temp_card['_id'])
+    card = temp_card
 
     return jsonify({'card': card})
+
+
+@app.route("/detail/<int:member_num>/<string:object_id>", methods=["POST"])
+def check_edit_password(member_num, object_id):
+    password_receive = request.form['password_give']
+
+    if password_receive == db.password.find_one({'member_num': member_num})['password']:
+        if member_num == 1:
+            temp_card = db.member_1.find_one({'_id': ObjectId(object_id)})
+        elif member_num == 2:
+            temp_card = db.member_2.find_one({'_id': ObjectId(object_id)})
+        elif member_num == 3:
+            temp_card = db.member_3.find_one({'_id': ObjectId(object_id)})
+        elif member_num == 4:
+            temp_card = db.member_4.find_one({'_id': ObjectId(object_id)})
+        elif member_num == 5:
+            temp_card = db.member_5.find_one({'_id': ObjectId(object_id)})
+        else:
+            temp_card = db.member_6.find_one({'_id': ObjectId(object_id)})
+
+        temp_card['_id'] = str(temp_card['_id'])
+        card = temp_card
+
+        return jsonify({'card': card, 'check': '1'})
+    else:
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.', 'check': '0'})
 
 
 @app.route("/detail/<int:member_num>/<string:object_id>", methods=["PUT"])
@@ -110,32 +179,49 @@ def edit_card_detail(member_num, object_id):
             'card_img': card_img_receive,
             'member_num': member_num
         }
-        db.member + \
-            member_num.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
-        return jsonify({'msg': '수정 완료'})
+
+        if member_num == 1:
+            db.member_1.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+        elif member_num == 2:
+            db.member_2.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+        elif member_num == 3:
+            db.member_3.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+        elif member_num == 4:
+            db.member_4.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+        elif member_num == 5:
+            db.member_5.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+        else:
+            db.member_6.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
+
+        return jsonify({'msg': '수정 완료', 'reload': '1'})
     else:
-        return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.', 'reload': '0'})
 
 
-# @app.route("/detail/<int:member_num>/<string:object_id>", methods=["PUT"])
-# def edit_card_detail(member_num, object_id):
-#     card_title_receive = request.form['card_title_give']
-#     card_text_receive = request.form['card_text_give']
-#     card_img_receive = request.form['card_img_give']
-#     password_receive = request.form['password_give']
+@app.route("/detail/<int:member_num>/<string:object_id>", methods=["DELETE"])
+def delete_card_detail(member_num, object_id):
+    password_receive = request.form['password_give']
 
-#     if password_receive == db.password.find_one({'member_num': member_num})['password']:
-#         doc = {
-#             'card_title': card_title_receive,
-#             'card_text': card_text_receive,
-#             'card_img': card_img_receive,
-#             'member_num': member_num
-#         }
-#         db.member + \
-#             member_num.update_one({'_id': ObjectId(object_id)}, {'$set': doc})
-#         return jsonify({'msg': '수정 완료'})
-#     else:
-#         return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
+    if password_receive == db.password.find_one({'member_num': member_num})['password']:
+
+        db.member_1.delete_one({'_id': ObjectId(object_id)})
+
+        if member_num == 1:
+            db.member_1.delete_one({'_id': ObjectId(object_id)})
+        elif member_num == 2:
+            db.member_2.delete_one({'_id': ObjectId(object_id)})
+        elif member_num == 3:
+            db.member_3.delete_one({'_id': ObjectId(object_id)})
+        elif member_num == 4:
+            db.member_4.delete_one({'_id': ObjectId(object_id)})
+        elif member_num == 5:
+            db.member_5.delete_one({'_id': ObjectId(object_id)})
+        else:
+            db.member_6.delete_one({'_id': ObjectId(object_id)})
+
+        return jsonify({'msg': '삭제 완료', 'reload': '1'})
+    else:
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.', 'reload': '0'})
 
 
 if __name__ == '__main__':
